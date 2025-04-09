d3c8f110566f0d538e2f289061ad21dd3bd93710
Udayendra Pal <109019105+Udayendra@users.noreply.github.com>
2025-04-09 16:23:34 +0530

    logic for MYSQL_HOST

--- src/config/config.php
+++ src/config/config.php
@@ -7,10 +7,13 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
+function isDocker(): bool {
+    return file_exists('/.dockerenv') || getenv('DOCKER') === 'true';
+}
 // Return config array
 return [
     'db' => [
-        'host' => $_ENV['MYSQL_HOST'] ?? 'db',
+        'host' => $_ENV['MYSQL_HOST'] ?? (isDocker() ? 'db' : '127.0.0.1'),
         'port' => 3306,
         'database' => $_ENV['MYSQL_DATABASE'],
         'user' => $_ENV['MYSQL_USER'],
